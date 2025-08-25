f8566d7f6896a3a968f60302ca9b1362407b72ec
Adachi Satoru <Adachi.Satoru@df.MitsubishiElectric.co.jp>
2025-08-25 13:15:03 +0900

    Remove unused forlder obj

--- ConsoleApp1.Tests/obj/Debug/net8.0/ConsoleApp1.Tests.AssemblyInfo.cs
+++ ConsoleApp1.Tests/obj/Debug/net8.0/ConsoleApp1.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5dd309afc3fe1e95494dfb56ba68383af021397")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc4a03a37bb39d0060dde5baab484b6f73afdbb2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
